dd1bd286576840671162f5090ad1c24e393d009e
Raymie <raymievsegarsiv@gmail.com>
2023-11-01 10:38:50 -0400

    Asignment 09 complete

---
+++ asgn09-CRUD/chain_gang/private/initialize.php
@@ -0,0 +1,33 @@
+<?php
+
+  ob_start();
+  define("PRIVATE_PATH", dirname(__FILE__));
+  define("PROJECT_PATH", dirname(PRIVATE_PATH));
+  define("PUBLIC_PATH", PROJECT_PATH . '/public');
+  define("SHARED_PATH", PRIVATE_PATH . '/shared');
+
+  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+  $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+  define("WWW_ROOT", $doc_root);
+
+  require_once('functions.php');
+  require_once('status_error_functions.php');
+  require_once('db_credentials.php');
+  require_once('database_functions.php');
+  require_once('validation_functions.php');
+
+  foreach(glob('classes/*.class.php') as $file) {
+    require_once($file);
+  }
+
+  function my_autoload($class) {
+    if(preg_match('/\A\w+\Z/', $class)) {
+      include('classes/' . $class . '.class.php');
+    }
+  }
+  spl_autoload_register('my_autoload');
+
+  $database = db_connect();
+  DatabaseObject::set_database($database);
+
+?>
