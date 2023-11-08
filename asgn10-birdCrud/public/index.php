29e56e803d95362fde433f727226ad6ca6f8bbfc
Raymie <raymievsegarsiv@gmail.com>
2023-11-08 13:43:00 -0500

    Actual asgn 10 submit

---
+++ asgn10-birdCrud/public/index.php
@@ -0,0 +1,46 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php
+
+$birds = Bird::find_all();
+  
+?>
+<?php $page_title = 'Birds'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="content">
+    <h1>Birds</h1>
+
+    <div class="actions">
+      <a class="action" href="<?php echo url_for('new.php'); ?>">Add Bird</a>
+    </div>
+
+  	<table class="list">
+      <tr>
+        <th>Common Name</th>
+        <th>Habitat</th>
+        <th>Food</th>
+        <th>Conservation Level</th>
+        <th>Backyard Tips</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+      </tr>
+
+      <?php foreach($birds as $bird) { ?>
+        <tr>
+          <td><?php echo h($bird->common_name); ?></td>
+          <td><?php echo h($bird->habitat); ?></td>
+          <td><?php echo h($bird->food); ?></td>
+          <td><?php echo h($bird->conservation_id); ?></td>
+          <td><?php echo h($bird->backyard_tips); ?></td>
+          <td><a class="action" href="<?php echo url_for('show.php?id=' . h(u($bird->id))); ?>">View</a></td>
+          <td><a class="action" href="<?php echo url_for('edit.php?id=' . h(u($bird->id))); ?>">Edit</a></td>
+          <td><a class="action" href="<?php echo url_for('delete.php?id=' . h(u($bird->id))); ?>">Delete</a></td>
+    	  </tr>
+      <?php } ?>
+  	</table>
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
+
